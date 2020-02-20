7869530a8155cf7b74216776143fa732dd30a17f
itswadesh <itswadesh@gmail.com>
2020-02-20 23:23:08 +0530

    fix: profile update on login

--- pages/my/profile.vue
+++ pages/my/profile.vue
@@ -1,195 +1,205 @@
 <template>
-  <div class="lg:w-1/2 mt-0 lg:mt-10 w-full bg-white shadow lg:ml-10 ml-0 py-5">
-    <div class="text-xl headings font-bold p-5 border-b border-gray-200 text-center items-center px-10">
-      <i
-        class="fa fa-arrow-left mr-2 block lg:invisible"
-        @click="$router.push('/my/')"
-        aria-hidden="true"
-      ></i>Profile details
+  <div>
+    <Heading title="Personal Details" />
+    <div class="w-full pb-4 lg:w-1/3 m-auto">
+      <form
+        class="lg:mx-15 form w-full mb-1"
+        novalidate
+        autocomplete="off"
+        @submit.stop.prevent="submit(profile)"
+      >
+        <div
+          class="p-2"
+          v-if="a"
+        >
+          <div
+            label="Phone"
+            class="w-full text-center mb-4"
+            name="name"
+          >Phone: {{a.phone}}
+          </div>
+          <div class="w-full flex justify-between">
+            <Textbox
+              label="First Name"
+              class="w-full"
+              name="firstName"
+              v-model="profile.firstName"
+            />
+            <Textbox
+              label="Last Name"
+              class="w-full"
+              name="lastName"
+              v-model="profile.lastName"
+            />
+          </div>
+          <Textbox
+            label="Address"
+            class="w-full"
+            name="name"
+            v-model="a.address"
+          />
+          <Textbox
+            label="Pin Code"
+            class="w-full"
+            name="name"
+            v-model="a.zip"
+          />
+          <Textbox
+            label="Town"
+            class="w-full"
+            name="name"
+            v-model="a.town"
+          />
+          <div class="w-full flex justify-between">
+            <Textbox
+              label="City"
+              class="w-1/2 mr-1"
+              name="name"
+              v-model="a.city"
+            />
+            <Textbox
+              label="State"
+              class="w-1/2 ml-1"
+              name="name"
+              v-model="a.state"
+            />
+          </div>
+
+          <SingleImageUpload
+            :image="profile.avatar"
+            name="avatar"
+            folder="avatar"
+            @remove="remove"
+            @save="save"
+          />
+        </div>
+        <div class="flex shadow lg:shadow-none fixed lg:relative bottom-0 justify-between w-full">
+          <button
+            type="button"
+            @click="$router.push('/my')"
+            class="tracking-widest p-3 w-1/2 bg-white text-black text-sm font-semibold lg:rounded"
+          >
+            CANCEL
+          </button>
+          <button
+            type="submit"
+            class="tracking-widest p-3 w-1/2 primary text-sm font-semibold lg:rounded"
+          >
+            CONTINUE
+          </button>
+        </div>
+      </form>
     </div>
-    <div class="px-0 lg:px-10">
-      <div class="w-full text-sm text-center mb-5 px-8">
-        <!-- Just 2 step(s) to go and your profile's complete -->
-        <div class="w-full h-1 bg-green-500 rounded-lg m-1 text-xs leading-none text-center text-white"></div>
-      </div>
-    </div>
-
-    <div class="w-full">
-      <ul class="w-full">
-        <div class="px-0 lg:px-10 hover:bg-gray-200">
-          <li class="flex-wrap flex mb-5 px-8">
-            <div class="w-1/12">
-              <img
-                src="/name.png"
-                class="w-5"
-                alt=""
-              />
-            </div>
-            <div class="w-10/12 flex">
-              <Textbox
-                v-model="profile.firstName"
-                label="First Name"
-                class=" mr-4 flex-1"
-              />
-              <Textbox
-                v-model="profile.lastName"
-                label="Last Name"
-                class="flex-1"
-              />
-            </div>
-            <!-- <div class="w-1/12">
-                <i
-                  class="fa fa-angle-right"
-                  aria-hidden="true"
-                ></i>
-              </div> -->
-          </li>
-        </div>
-
-        <div class="px-0 lg:px-10 hover:bg-gray-200">
-          <li class="flex-wrap flex mb-5 hover:bg-gray-200 px-8">
-            <div class="w-1/12 text-xl">
-              <i
-                class="fa fa-mobile"
-                aria-hidden="true"
-              ></i>
-            </div>
-            <div class="w-10/12">
-              <Textbox
-                v-model="profile.phone"
-                label="Phone"
-              />
-            </div>
-            <!-- <div class="w-1/12">
-                <i
-                  class="fa fa-angle-right"
-                  aria-hidden="true"
-                ></i>
-              </div> -->
-          </li>
-        </div>
-
-        <div class="px-0 lg:px-10 hover:bg-gray-200">
-          <li class="flex-wrap flex mb-5 hover:bg-gray-200 px-8">
-            <div class="w-1/12">
-              <i
-                class="fa fa-envelope-o"
-                aria-hidden="true"
-              ></i>
-            </div>
-            <div class="w-10/12">
-              <Textbox
-                v-model="profile.email"
-                label="Email"
-              />
-            </div>
-            <!-- <div class="w-1/12">
-                <i
-                  class="fa fa-angle-right"
-                  aria-hidden="true"
-                ></i>
-              </div> -->
-          </li>
-        </div>
-
-        <div class="px-0 lg:px-10 hover:bg-gray-200">
-          <nuxt-link to="/my/password">
-            <li class="flex-wrap flex mb-5 hover:bg-gray-200 py-5 px-8">
-              <div class="w-1/12">
-                <i
-                  class="fa fa-key"
-                  aria-hidden="true"
-                ></i>
-              </div>
-              <div class="w-10/12">Password</div>
-              <div class="w-1/12">
-                <i
-                  class="fa fa-angle-right"
-                  aria-hidden="true"
-                ></i>
-              </div>
-            </li>
-          </nuxt-link>
-        </div>
-      </ul>
-    </div>
+    <GeoLocation />
   </div>
 </template>
 
-
 <script>
-import { mapGetters, mapActions } from "vuex";
-import Textbox from "~/components/ui/Textbox";
-
+import { mapActions } from "vuex";
+const Heading = () => import("~/components/Heading");
+const Textbox = () => import("~/components/ui/Textbox");
+const GeoLocation = () => import("~/components/GeoLocation");
+const SingleImageUpload = () => import("~/components/SingleImageUpload");
+import { location } from "~/mixins";
 export default {
   fetch({ store, redirect }) {
     if (!(store.state.auth || {}).user)
-      return redirect("/login?return=my/profile");
+      return redirect("/login?return=/my/profile");
   },
-  async asyncData({ store }) {},
+  mixins: [location],
   data() {
     return {
-      user: null,
-      showImageModal: false,
-      userAvatar: null,
-      states: [],
-      cities: [],
-      dd: null,
-      mm: null,
-      yyyy: null
+      a: {},
+      profile: {}
     };
   },
+  components: {
+    Heading,
+    Textbox,
+    GeoLocation,
+    SingleImageUpload
+  },
   computed: {
-    ...mapGetters({
-      loading: "loading"
-    })
+    // user() {
+    //   return (this.$store.state.auth || {}).user || null;
+    // }
   },
-  components: { Textbox },
   async created() {
-    let profile = {};
-    let userDetails = await this.$store.dispatch("auth/fetch");
-    profile = Object.assign({}, userDetails);
-    profile.dob = profile.dob || {};
-    profile.state = profile.state || {};
-    this.profile = profile;
-  },
-  async mounted() {
-    this.getStates(this.profile.country);
-    this.getCities(this.profile.state);
+    // await this.$axios.$get(`/api/geo/remove`);
+    // this.$cookies.remove("geo");
+    try {
+      this.$store.commit("busy", true);
+      this.user = await this.$axios.$get(`api/users/me`);
+      this.profile = { ...this.user };
+      this.a = this.$cookies.get("geo");
+      this.profile.address = this.profile.address || {};
+      this.a.address =
+        this.profile.address.address || (this.a && this.a.address);
+      this.a.town = this.profile.address.county || (this.a && this.a.county);
+      this.a.city =
+        this.profile.address.city || (this.a && this.a.state_district);
+      this.a.zip = this.profile.address.zip || (this.a && this.a.postcode);
+      this.a.firstName =
+        this.profile.address.firstName || this.profile.firstName;
+      this.a.lastName = this.profile.address.lastName || this.profile.lastName;
+      this.a.phone = this.profile.phone;
+    } catch (e) {
+    } finally {
+      this.$store.commit("busy", false);
+    }
   },
   methods: {
-    async getCities(state) {
-      this.cities = await this.$axios.$get("api/countries/cities", {
-        params: { state }
-      });
+    save(name, image) {
+      this.profile.avatar = image;
+      // this.submit();
     },
-    async getStates() {
-      this.states = await this.$axios.$get("api/countries/states");
+    remove(name) {
+      this.profile.avatar = "";
     },
     ...mapActions({
       updateProfile: "auth/updateProfile"
     }),
-    splitDate(date) {
-      if (!date) return {};
-      var dd = date.getDate();
-      var mm = date.getMonth() + 1; //January is 0!
-      var yyyy = date.getFullYear();
-      if (dd < 10) {
-        dd = "0" + dd;
+    go(url) {
+      this.$router.push(url);
+    },
+    async submit(profile) {
+      try {
+        this.$store.commit("busy", true);
+        this.profile.address = this.a;
+        // const data = await this.$axios.$put("api/users/profile", this.profile);
+        await this.updateProfile(this.profile);
+        this.go("/my");
+      } catch (e) {
+      } finally {
+        this.$store.commit("busy", false);
       }
-      if (mm < 10) {
-        mm = "0" + mm;
-      }
-      var date = { dd, mm, yyyy };
-      return date;
     }
   },
-  head() {
-    return {
-      title: "Update your profile"
-    };
-  },
-  layout: "account"
+  layout: "none"
 };
 </script>
 
+<style scoped>
+/* form */
+form {
+  display: inline-block;
+}
+.field {
+  padding-top: 10px;
+  display: flex;
+  flex-direction: column;
+}
+label {
+  order: -1;
+  padding-left: 5px;
+  font-size: 14px;
+  transition: all 0.3s ease-in;
+  transform: translateY(30px);
+  pointer-events: none;
+}
+input:focus + label,
+textarea:focus + label {
+  transform: translateY(16px);
+}
+/* form ends */
+</style>
